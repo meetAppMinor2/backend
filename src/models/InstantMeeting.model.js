309524f2ac95a4cb5e097308d020ebd7a12960de
Harsh Singh <harsh.101783@stu.upes.ac.in>
2025-02-01 23:27:36 +0530

    Instant meet scehma created

--- src/models/InstantMeeting.model.js
+++ src/models/InstantMeeting.model.js
@@ -1,52 +1,37 @@
-// import mongoose, { Schema } from "mongoose";
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "./user.model.js";
 
-// const InstantMeetingSchema = new Schema({
-//     meeting_id:{
-//         type: String,
-//         required: [true,"Meeting ID is required"],
-//         unique: true,
-//         trim: true,
-//         index: true
-//     },
-//     host: {
-//         type: String,
-//         required: [true, "Host is required."],
-//         trim: true,
-//     },
+const InstantMeetingSchema = new Schema({
+    host:{
+        type:Schema.Types.ObjectId,
+        ref: User,
+        required:true
+    },
 
-//     members: {
-//         type: [String],
-//         required: [true, "Member is required."],
-//         validate: {
-//             validator: function (members) {
-//                 return members.length > 0;
-//             },
-//             message: "At least one member is required."  
-//         },
-//         trim: true,
-//     },
-//     // duration: {
-//     //     type: Number,
-//     //     required: [true, "Duration is required."],
-//     //     min: [1, "at least 1 minute"],
-//     // },
-//     recordings: {
-//         type: [Stirng],
-//         default: []
-//     },
-//     created_at: {
-//         type: Date,
-//         default: Date.now
-//     },
-    
-//     updated_at: {
-//         type: Date,
-//         default: Date.now
-//     }
+    memebrs:[{
+        type:Schema.Types.ObjectId,
+        ref: User ,
+        required:true
+    }],
 
-// });
+    duration:{
+        type:String,
+        required:true
+    },
 
+    recording:{
+        type:[String],
+        default:[]
+    },
 
-// export const InstantMeeting = mongoose.model("InstantMeeting", InstantMeetingSchema);
+    meetingCode:{
+        type:String,
+        default:NULL,
+        required:true
+    }
+},{timestamps:true});
+
+const InstantMeeting=mongoose.model('InstantMeeting',InstantMeetingSchema);
+export default InstantMeeting;
